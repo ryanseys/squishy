12cf57deb39776547496ebdedcef23d5f1d15340
Ryan Seys <ryan@ryanseys.com>
2013-10-02 23:53:44 -0400

    Initial 0.0.1 squishy

---
+++ lib/squishy.rb
@@ -0,0 +1,27 @@
+class Squishy
+  def self.squish(site_dir)
+    jar_file_path = File.join(File.dirname(__FILE__), *%w"htmlcompressor.jar")
+    puts "\nSquishing..."
+    html_options = [
+      '--type html', # squish html files
+      '--remove-intertag-spaces', # squish moar!
+      '--compress-js', # squish inline js (yui)
+      '--compress-css' # squish inline css (yui)
+    ]
+
+    xml_options = [
+      '--type xml' # and squish xml files
+    ]
+
+    options = [
+      "#{jar_file_path}", # path to squisher jar
+      '-r', # recursivly find and squish all files
+      "#{site_dir}", # the directory we read from
+      "-o #{site_dir}" # output directory
+    ]
+
+    system("java -jar #{[options, html_options].flatten.join(' ')}")
+    system("java -jar #{[options, xml_options].flatten.join(' ')}")
+    puts "\nSquished!"
+  end
+end
---
+++ squishy.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'squishy'
+  s.version     = '0.0.1'
+  s.date        = '2013-10-02'
+  s.summary     = "Squish stuff!"
+  s.description = "Squish your html and xml files"
+  s.authors     = ["Ryan Seys"]
+  s.email       = 'ryan@ryanseys.com'
+  s.files       = ["lib/squishy.rb", "lib/htmlcompressor.jar", "lib/yuicompressor.jar"]
+  s.homepage    = 'http://rubygems.org/gems/squishy'
+  s.license     = 'MIT'
+end
